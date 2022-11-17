918a8bd4a95aa19ef5b4c1ded4e59652767b4126
JustAyce <kahowe1woo@gmail.com>
2022-11-17 11:15:26 +0800

    modified ip at apptest

--- src/test/java/mycompany/app/AppTest.java
+++ src/test/java/mycompany/app/AppTest.java
@@ -22,8 +22,8 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class AppTest {
 	WebDriver driver;
 	WebDriverWait wait;
-	String url = "http://192.168.0.228";
-	String validEmail = "kahowe1woo@gmail.com";
+	String url = "http://172.17.0.2";
+	String validEmail = "user@example.com";
 	String validPassword = "password1234";
 	String invalidEmail = "none@example.com";
 	String invalidPassword = "password";
